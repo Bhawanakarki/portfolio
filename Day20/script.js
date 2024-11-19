c4143ab1122f08a978e13259eb8cdc654b26555f
unknown <karkibhawana24@gmail.com>
2024-11-19 13:43:02 +0545

    first commit

---
+++ Day20/script.js
@@ -0,0 +1,11 @@
+  
+    var typed= new typed(".work",{
+    strings:["Professional Coder","Developer", "UI/UX Designer"],
+    typeSpeed:50,
+    backSpeed:50,
+    looped:true
+   
+
+    })
+
+   
